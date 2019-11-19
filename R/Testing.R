8e8701ef5807e0841cbe46b6bc0e6dfbd3fc18a6
Alex Garcia <alex.garcia@fitbit.com>
2019-11-19 14:09:32 -0800

    Cleaning up extraneous files

---
+++ R/Testing.R
@@ -0,0 +1,25 @@
+# For testing
+
+# Date 11.04
+directory = "~/Desktop/FHS/Coaching/Data/November/11.4"
+date.first.day = "11_01_2019"
+date.current.report = "11_04_2019"
+
+data_11_04 <- clean_merge_data()
+
+# Date 11.11
+directory = "~/Desktop/FHS/Coaching/Data/November/11.11"
+date.first.day = "11_01_2019"
+date.current.report = "11_11_2019"
+
+data_11_11 <- clean_merge_data()
+
+# Date 11.11
+directory = "~/Desktop/FHS/Coaching/Data/November/11.18"
+date.first.day = "11_01_2019"
+date.current.report = "11_18_2019"
+
+data_11_18 <- clean_merge_data()
+
+
+
