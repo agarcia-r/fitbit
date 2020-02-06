b887a4113eb6bb0088c293a4cd1e4671a0e4f89a
Alex Garcia <alex.garcia@fitbit.com>
2020-02-06 13:58:38 -0800

    Adding make_coach_report() function

---
+++ R/make_coach_report.R
@@ -0,0 +1,14 @@
+#' Make Coach Engagement Reports
+#'
+#' This function takes a cleaned billing file and creates the weekly engagement report for a specific coach.
+#' @keywords coach engagement report
+#' @export make_coach_report
+#' @examples
+#' make_coach_report()
+
+make_coach_report <- function(data, coach, dir = getwd(), date.current = date.current.report){
+  data <- data[data$Primary.Coach == coach & data$Enrollment.Current == T & data$Engaged.Status == F, ]
+  data <- data[ , c("Organization.Name", "Fitbit.ID", "Enrollment.Current", "Engaged.Status", "Last.Message.From.Member.At")]
+  write.csv(data, file = paste(dir, "/Coach_", coach, "_", date.current.report, ".csv", sep = ""))
+  return(data)
+}
